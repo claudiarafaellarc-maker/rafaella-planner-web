ae4ef5d8afbc0abe531db9624c339d32784cc87e
claudiarafaellarc-maker <2.8848107e+08+claudiarafaellarc-maker@users.noreply.github.com>
2026-07-02 14:53:07 +0000

    Updates

---
+++ sw.js
@@ -0,0 +1,122 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "c2ec3c2a6f6cbdff6a7abc2b114585ca"
+  }, {
+    "url": "index.html",
+    "revision": "ce880f74d0411f4c9b681f5a9862b1d6"
+  }, {
+    "url": "icons.svg",
+    "revision": "3b4fcfcf393eca4d264dca4a4663bc37"
+  }, {
+    "url": "icon-512.png",
+    "revision": "224b6394a9c16e70eb67edae91ad585c"
+  }, {
+    "url": "icon-192.png",
+    "revision": "a7d49307ba5401c29d132a691425fb03"
+  }, {
+    "url": "favicon.svg",
+    "revision": "7e840862161341271697daa99a40d76b"
+  }, {
+    "url": "apple-touch-icon.png",
+    "revision": "b702b865777c023decbec9081001db8f"
+  }, {
+    "url": "assets/index-BdGiOqJH.js",
+    "revision": null
+  }, {
+    "url": "assets/index-BBp9YlFM.css",
+    "revision": null
+  }, {
+    "url": "apple-touch-icon.png",
+    "revision": "b702b865777c023decbec9081001db8f"
+  }, {
+    "url": "icon-192.png",
+    "revision": "a7d49307ba5401c29d132a691425fb03"
+  }, {
+    "url": "icon-512.png",
+    "revision": "224b6394a9c16e70eb67edae91ad585c"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "241d78ced04d11d18dbbd6ccd614fb1f"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
